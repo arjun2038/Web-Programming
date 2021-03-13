90be56e9ddba445440ded597dc3739b308271ac8
ARJUN V PANKAJAKSHAN <2038@tkmce.ac.in>
2021-03-13 17:11:41 +0530

    co4 q2 updatedd

--- Course Outcome 4/Q2/electricitybill.php
+++ Course Outcome 4/Q2/electricitybill.php
@@ -1,34 +1,53 @@
 <html>
-<center><body>
-
+<center>
+<body>
 <h1>Customer Electricity Bill Request Form</h1>
 <form method="post" action="#">
 <table><tr><td>Name:</td><td><input type="text" name="name"></td></tr>
-<tr><td>Units Consumed :</td><td><input type="number" name="units"></td></tr>
-<tr><td>Rate Per Unit :</td><td><input type="number" name="rate"></td></tr></table>
-<tr><td colspan=2><input type="submit"></td></tr>
+<tr><td>Consumer Number</td><td><input type="number" name="cnumber"></td></tr>
+<tr><td>Mobile Number</td><td><input type="number" name="mnumber"></td></tr>
+<tr><td>Previous Reading</td><td><input type="number" name="pr"></td></tr>
+<tr><td>Current Reading :</td><td><input type="number" name="cr"></td></tr>
+<tr><td colspan=2 align="middle"><input type="submit"></td></tr></table>
 </form>
 <?php
+  $name =$creading =$preading =$mobile=$consumer=$units ="";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST['name'];
-  $units = $_POST['units'];
-  $rate = $_POST['rate'];
-if($name!="" and $units!="" and $rate!="")
+  $creading = $_POST['cr'];
+  $preading = $_POST['pr'];
+  $mobile=$_POST['mnumber'];
+  $consumer=$_POST['cnumber'];
+  $units =floatval($creading)-floatval($preading);
+if($units<100)
 {
+  $rate=3;
+  $amount=$units*$rate;
+}
+elseif($units<=250)
+{
+  $rate=4;
+  $amount=$units*$rate;
+}
+elseif($units>250)
+{
+  $rate=6;
+  $amount=$units*$rate;
+}
+if($name!="" and $preading!="" and $rate!="" and $creading!="" and $consumer!="" and $mobile!=""){
   echo "-------------------------------------------------------------------------";
   echo "<h3>Kerala State Electricity Board (KSEB)</h3>";
   echo "<br><br>";
   echo "Consumer name : ".$name."<br>";
+  echo "Consumer No   : ".$consumer."<br>";
   echo "Units Consumed : ".$units."<br>";
   echo "Rate per unit : Rs.".$rate."<br><br>";
 
-  echo "<h4>Total : Rs.".($rate*$units)."</h4><br>";
+  echo "<h4>Total : Rs.".($amount)."</h4><br>";
   echo "-------------------------------------------------------------------------";
-
 }
-else {
+else 
   echo "Some Field are missing,Enter properly.";
-}
 }
 ?>
 </body></center>
